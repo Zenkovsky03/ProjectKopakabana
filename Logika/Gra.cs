497d1b018a4de653b9af8d74e155ddcef20a6ae8
maciekiwaniuk <iwaniukmaciej.kontakt@gmail.com>
2024-06-15 19:30:05 +0200

    Update logic

--- Logika/Gra.cs
+++ Logika/Gra.cs
@@ -6,37 +6,9 @@ namespace Kopakabana
 {
     public class Gra
     {
-        private int Id { get; set; }
-        private DateTime Data { get; set; }
-        private List<Druzyna> Druzyny { get; set; }
-        private List<AbstractSedzia> Sedziowie { get; set; }
-        private string Wynik { get; set; }
-
-        public void ZaplanujGre()
-        {
-            // TODO: 
-        }
-        
-        public void ZapiszWynik()
-        {
-            var druzyna = new
-            {
-                Id,
-                Data,
-                Druzyny,
-                Sedziowie,
-                Wynik
-            };
-
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = true
-            };
-
-            string json = JsonSerializer.Serialize(druzyna, options);
-            string fileName = $"Gra-{Id}-{Data}.json";
-
-            File.WriteAllText(fileName, json);
-        }
+        public List<Druzyna> Druzyny { get; set; }
+        public List<AbstractSedzia> Sedziowie { get; set; }
+        public Druzyna Wygrany { get; set; }
+        public string Wynik { get; set; }
     }
 }
