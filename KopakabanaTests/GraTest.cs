c59843d552710ee7ef8f9ac55f271f38262ebfb6
macieeek <iwaniukmaciej.kontakt@gmail.com>
2024-06-20 23:23:27 +0200

    Add game test

---
+++ KopakabanaTests/GraTest.cs
@@ -0,0 +1,17 @@
+﻿using Kopakabana;
+
+namespace KopakabanaTests
+{
+    public class GraTest
+    {
+        [Fact]
+        public void StworzenieGryTest()
+        {
+            Druzyna druzyna1 = new Druzyna();
+            Druzyna druzyna2 = new Druzyna();
+            Gra gra = new Gra(druzyna1, druzyna2);
+
+            Assert.True(gra.Druzyny.Count == 2);
+        }
+    }
+}
